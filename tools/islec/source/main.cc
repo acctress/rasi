4ac6c53cb9c25bce5d3bfadbf9ff3da4c79e2d12
acctress <blinxduh@gmail.com>
2026-08-21 22:49:09 +0100

    feat: added compiler generation comment at the top of the lower function, fixed the switch branches formatting, changed indent to be 4 spaces, added input and output args to `islec` compiler tool.

--- tools/islec/source/main.cc
+++ tools/islec/source/main.cc
@@ -4,76 +4,50 @@
 #include "parser.hh"
 
 #include <exception>
+#include <fstream>
 #include <iostream>
+#include <print>
 #include <string_view>
 #include <vector>
-#include <print>
 
-int main()
+int main( int argc, char *argv[] )
 {
-    constexpr std::string_view source = R"(
-(type Value (primitive))
+    if ( argc < 2 ) return 1;
 
-(decl extractor iadd  (Value Value) Value)
-(decl extractor isub  (Value Value) Value)
-(decl extractor imul  (Value Value) Value)
-(decl extractor idiv  (Value Value) Value)
-(decl extractor neg  (Value) Value)
-(decl extractor load (Value) Value)
+    const auto input_filename  = std::string { argv[ 1 ] };
+    const auto output_filename = std::string { argv[ 2 ] };
 
-(decl constructor emit_add  (Value Value) Value)
-(decl constructor emit_sub  (Value Value) Value)
-(decl constructor emit_mul  (Value Value) Value)
-(decl constructor emit_div  (Value Value) Value)
-(decl constructor emit_neg  (Value) Value)
-(decl constructor emit_load (Value) Value)
-
-(rule 100 (iadd lhs rhs)
-          (emit_add lhs rhs))
-
-(rule 90 (isub lhs rhs)
-         (emit_sub lhs rhs))
-
-(rule 80 (imul lhs rhs)
-         (emit_mul lhs rhs))
-
-(rule 70 (idiv lhs rhs)
-         (emit_div lhs rhs))
-
-(rule 60 (neg value)
-         (emit_neg value))
-
-(rule 50 (load address)
-         (emit_load address))
-)";
-
-    std::println("\x1b[38;5;217mbasic_rule_test.isle:\x1b[0m\n{}", source);
-    std::println("\x1b[38;5;217mgenerated matches:\x1b[0m");
+    std::ifstream file { input_filename };
+    std::string   input_source( ( std::istreambuf_iterator( file ) ), std::istreambuf_iterator< char >( ) );
 
     try
     {
-        parser p { source };
-        const auto ast = p.parse();
+        parser     p { input_source };
+        const auto ast = p.parse( );
 
         checker ch { ast };
-        ch.check();
+        ch.check( );
 
-        std::vector<const rule_node*> rules;
+        std::vector< const rule_node * > rules;
 
-        for (const auto& node : ast)
+        for ( const auto &node : ast )
         {
-            if (const auto* rule = std::get_if<rule_node>(&node))
-                rules.push_back(rule);
+            if ( const auto *rule = std::get_if< rule_node >( &node ) ) rules.push_back( rule );
         }
 
-        const auto root = dec_tree::build(std::move(rules));
+        const auto root = dec_tree::build( std::move( rules ) );
 
-        emitter out { root, std::cout };
-        out.emit();
+        std::ofstream output { output_filename };
+        if ( !output.is_open( ) ) throw std::runtime_error( "Could not open output file" );
+
+        emitter out { root, output };
+        out.emit( );
+
+        output.close( );
     }
-    catch (const std::exception& error)
+    catch ( const std::exception &error )
     {
-        std::cerr << "error: " << error.what() << '\n';
+        std::cerr << "error: " << error.what( ) << '\n';
         return 1;
     }
 }
