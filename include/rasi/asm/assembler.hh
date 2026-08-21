9e6c151999227e1a150d22478fec4afce8134596
acctress <blinxduh@gmail.com>
2026-08-22 00:52:44 +0100

    feat: created instructions, encoding, assembler skeleton, moved buffer.

--- include/rasi/asm/assembler.hh
+++ include/rasi/asm/assembler.hh
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "buffer.hh"
+#include "rasi/ir/instruction.hh"
+
+#include <rasi/asm/encoding.hh>
+#include <rasi/asm/operand.hh>
+#include <rasi/support/types.hh>
+#include <unordered_map>
+#include <vector>
+
+namespace rasi::asm_
+{
+    struct MachInst
+    {
+        InstKind               kind {};
+        std::vector< Operand > operands {};
+        OperandConstraint      output_constraint {};
+        VReg                   result {};
+    };
+
+    class Assembler
+    {
+    public:
+        void emit_bytes( const std::unordered_map< VReg, PhysReg > &alloc, Buffer &buf );
+
+        VReg iadd( VReg lhs, VReg rhs ) noexcept;
+        VReg isub( VReg lhs, VReg rhs ) noexcept;
+        VReg imul( VReg lhs, VReg rhs ) noexcept;
+        void ret( VReg value ) noexcept;
+
+    private:
+        std::vector< MachInst > m_insts {};
+        u32                     m_next_vreg {};
+
+        VReg vreg( ) { return VReg { m_next_vreg++ }; }
+    };
+} // namespace rasi::asm_
--- include/rasi/asm/operand.hh
+++ include/rasi/asm/operand.hh
@@ -1,1 +1,55 @@
 #pragma once
+
+#include "registers.hh"
+
+#include <rasi/support/types.hh>
+
+namespace rasi::asm_
+{
+    struct VReg
+    {
+        u32 id;
+    };
+
+    enum class OperandKind : u8
+    {
+        reg,
+        imm,
+        mem
+    };
+
+    enum class ConstraintKind : u8
+    {
+        any,
+        fixed,
+        reuse
+    };
+
+    struct Operand
+    {
+        OperandKind kind {};
+
+        union
+        {
+            VReg reg;
+            i64  imm;
+
+            struct
+            {
+                VReg base;
+                i32  offset;
+            } mem;
+        };
+    };
+
+    struct OperandConstraint
+    {
+        ConstraintKind kind { ConstraintKind::any };
+
+        union
+        {
+            PhysReg fixed_reg;
+            u8      reuse_index;
+        };
+    };
+} // namespace rasi::asm_
