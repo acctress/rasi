a268cda478dbea2b69705dd9eddc2872a2192ac5
acctress <blinxduh@gmail.com>
2026-08-22 12:51:13 +0100

    feat: created linear_scan.hh/.cc definitions and implementation placeholders

--- include/rasi/regalloc/linear_scan.hh
+++ include/rasi/regalloc/linear_scan.hh
@@ -1,1 +1,33 @@
 #pragma once
+#include "rasi/asm/operand.hh"
+#include "rasi/asm/vcode.hh"
+
+#include <unordered_map>
+#include <vector>
+
+using namespace rasi::asm_;
+
+namespace rasi::regalloc
+{
+    struct LiveInterval
+    {
+        VReg    reg;
+        PhysReg assigned;
+        u32     start;
+        u32     end;
+    };
+
+    class LinearScan
+    {
+    public:
+        std::unordered_map< u32, PhysReg > run( const VCode &vcode );
+
+    private:
+        std::vector< LiveInterval >        m_intervals;
+        std::unordered_map< u32, PhysReg > m_alloc;
+
+        void build_intervals( const VCode &vcode );
+        void alloc( );
+    };
+
+} // namespace rasi::regalloc
---
+++ lib/regalloc/linear_scan.cc
@@ -0,0 +1,9 @@
+#include <rasi/regalloc/linear_scan.hh>
+
+using namespace rasi::regalloc;
+
+std::unordered_map< u32, PhysReg > LinearScan::run( const VCode &vcode ) { }
+
+void LinearScan::build_intervals( const VCode &vcode ) { }
+
+void LinearScan::alloc( ) { }
